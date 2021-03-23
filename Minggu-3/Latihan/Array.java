668b913b143c8745d86d224071fd21e2a238947a
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 11:51:05 +0700

    Create Array.java

---
+++ Minggu-3/Latihan/Array.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author User
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] angka = {3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("Jumlah elemen array angka ="+angka.length);
+       
+    }
+    
+}
