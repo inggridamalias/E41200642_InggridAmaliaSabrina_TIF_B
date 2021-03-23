9457514a8c5d298b5da9244b0e39c3f05005f09b
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 11:52:26 +0700

    Create tugas3.java

---
+++ Minggu-3/Tugas/tugas3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas5;
+
+/**
+ *
+ * @author User
+ */
+public class tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [][] angka = {{5,6,1,7},{8,1,2,9},{5,4,7,1},{8,3,7,5}};
+        for (int baris = 0; baris < 4; baris++) {
+            for (int kolom = 0; kolom < 4; kolom++) {
+                System.out.print(angka[baris][kolom]+ "   ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
