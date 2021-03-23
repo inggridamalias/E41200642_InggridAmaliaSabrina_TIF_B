fd875a68e2e953b957d46396b1d2de5b9746d333
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 11:52:08 +0700

    Create tugas1.java

---
+++ Minggu-3/Tugas/tugas1.java
@@ -0,0 +1,22 @@
+
+package Tugas5;
+
+/**
+ *
+ * @author User
+ */
+public class tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama[] = {"Inggrid","Amalia", "Sabrina"};
+        
+        for(int j=0; j<nama.length; j++){
+            System.out.print(nama[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
