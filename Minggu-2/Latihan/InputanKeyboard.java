0420ba1c5b6a900a18b36569502c77b4d9a34b96
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 17:22:11 +0700

    Create InputanKeyboard.java

---
+++ Minggu-2/Latihan/InputanKeyboard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+import java.util.Scanner;
+
+public class InputanKeyboard {
+
+    public static void main(String[] args) {
+// Buat scanner
+        Scanner s1 = new Scanner(System.in);
+        
+// Ambil nilai dari keyboard
+        System.out.print("Masukkan nilai1 = ");
+        int nilai1 = s1.nextInt();
+        System.out.print("Masukkan nilai2 = ");
+        int nilai2 = s1.nextInt();
+
+// Hitung dan Print
+        int jumlah = nilai1 + nilai2;
+        System.out.println("Jumlah = " + jumlah);
+    }
+    
+}
