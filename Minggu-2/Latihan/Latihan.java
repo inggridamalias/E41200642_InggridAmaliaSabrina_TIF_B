2545e8af04fafc08516e93a84bf393a7b6faa7a3
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 17:22:14 +0700

    Create Latihan.java

---
+++ Minggu-2/Latihan/Latihan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World!");
+        
+        System.out.print("Hello");
+        System.out.print("World!");
+    }
+    
+}
