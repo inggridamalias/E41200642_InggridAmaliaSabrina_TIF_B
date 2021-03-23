1b7c6a901c4ae593d17127b0807478478bd77d71
inggridamalias <inggridamalias99@gmail.com>
2021-03-23 11:52:22 +0700

    Create tugas2.java

---
+++ Minggu-3/Tugas/tugas2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas5;
+
+/**
+ *
+ * @author User
+ */
+public class tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] number = {2, 19, 8, 22,1};
+        
+        for(int i=0; i<number.length; i++){
+            System.out.println(number[i]);
+        }
+    }
+    
+}
