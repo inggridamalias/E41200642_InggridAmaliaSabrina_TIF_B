fd7555a889ec0059229088067d5f0063a49e78fc
inggridamalias <inggridamalias99@gmail.com>
2021-04-10 20:33:28 +0700

    Create Vertex.java

---
+++ Minggu-5/Graph/Vertex.java
@@ -0,0 +1,15 @@
+package Graph;
+
+/**
+ *
+ * @inggridamaliasabrina
+ */
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label) {
+        this.label = label; wasVisited = false;
+    }
+    
+}
