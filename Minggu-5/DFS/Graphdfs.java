2dbaba802336b9f50fe790c7efa9005c0d17a749
Inggrid Amalia Sabrina <80300877+inggridamalias@users.noreply.github.com>
2021-04-10 22:03:30 +0700

    Update Graphdfs.java

--- Minggu-5/DFS/Graphdfs.java
+++ Minggu-5/DFS/Graphdfs.java
@@ -21,29 +21,24 @@ public class Graphdfs {
             }
         }
         theStack = new Stackdfs();
-       
+    }   
     public void addVertexdfs(char label) { 
         vertexList[nVerts++] = new Vertexdfs(label);
     }
-    
     public void addEdge(int start, int end) {
         adjMat[start][end] = 1;
         adjMat[end][start] = 1;
     }
-    
     public void displayVertex(int v) {
         System.out.print(vertexList[v].label);
     }
-    
     public void displayVertexdfs(int v) {
         System.out.print(vertexList[v].label);
     }
-
     public void dfs(){
         vertexList[0].wasVisited = true;
         displayVertex(0);
         }
-
     void addVertex(char c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
