5a9d6ccd6993079e72fbfe2dc8ada1903a6747a3
inggridamalias <inggridamalias99@gmail.com>
2021-04-10 20:32:54 +0700

    Create Node.java

---
+++ Minggu-5/BinaryTree/Node.java
@@ -0,0 +1,17 @@
+package BinaryTree;
+
+/**
+ *
+ * @inggridamaliasabrina
+ */
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode() {
+        System.out.print("{ " + id + ", " + data + " } ");
+    }
+    
+}
