227a12ce722fdc7048692d302109ffc6a6b1e4de
inggridamalias <inggridamalias99@gmail.com>
2021-04-10 20:33:14 +0700

    Create Vertexdfs.java

---
+++ Minggu-5/DFS/Vertexdfs.java
@@ -0,0 +1,17 @@
+package DFS;
+
+/**
+ *
+ * @inggridamaliasabrina
+ */
+public class Vertexdfs {
+   public char label;
+    public boolean wasVisited;
+    
+    public Vertexdfs(char label) {
+        this.label = label; 
+        wasVisited = false;
+    }
+    
+}
+
