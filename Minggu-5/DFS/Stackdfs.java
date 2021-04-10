3dd35801ca4e4b70b804a02c3d2003309104f0e7
inggridamalias <inggridamalias99@gmail.com>
2021-04-10 20:33:11 +0700

    Create Stackdfs.java

---
+++ Minggu-5/DFS/Stackdfs.java
@@ -0,0 +1,27 @@
+package DFS;
+
+/**
+ *
+ * @inggridamaliasabrina
+ */
+public class Stackdfs {
+    private final int SIZE = 20;
+    private int[] st;
+    private int top;
+    
+        public Stackdfs() {
+        st = new int[SIZE];
+        top = -1;
+        }
+    public void push(int j)
+        { st[++top] = j; }
+    
+    public int pop()
+        { return st[top--]; }
+    
+    public int peek()
+        { return st[top]; }
+    
+    public boolean isEmpty()
+        {return (top == -1); }
+}
