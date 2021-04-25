bb9732898c426750041db141ddf7eb9525069028
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:03:25 +0700

    Create B.java

---
+++ Minggu-7/Latihan/Abstract/B.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public class B extends A {
+    public void method2(){ // Method abstractdi-override dan dibuat
+        System.out.println("Method abstract yang sudah menjadi konkrit dalam class B");
+    }  
+}
