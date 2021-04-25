f16c4106dd2eee18ce81344b4434f9e45c458769
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:20:58 +0700

    Create C.java

---
+++ Minggu-7/Tugas/TugasB/C.java
@@ -0,0 +1,11 @@
+package TugasB;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public class C extends A{
+    public void callthis(){
+        System.out.println("Inside Class C's Method!");
+    }
+}
