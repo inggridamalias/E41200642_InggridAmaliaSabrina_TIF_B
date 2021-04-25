d3a749aaf9ff52b9406f126b6bc97a5e57c7b34a
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:21:17 +0700

    Create Dog.java

---
+++ Minggu-7/Tugas/TugasC/Dog.java
@@ -0,0 +1,12 @@
+package TugasC;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public class Dog extends Animal{
+    public void makeNoise(){
+        System.out.println("bark");
+        
+    }
+}
