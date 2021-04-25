f1caee55996c9c2b095e3b110be9cb670a7c7307
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:03:16 +0700

    Create A.java

---
+++ Minggu-7/Latihan/Abstract/A.java
@@ -0,0 +1,13 @@
+package Abstract;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public abstract class A { //Deklarasi Class
+    abstract public void method2();
+    // method konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+}
