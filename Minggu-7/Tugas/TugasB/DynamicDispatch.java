fac4920d3784d7b6bdc73148232e705c88936778
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:21:02 +0700

    Create DynamicDispatch.java

---
+++ Minggu-7/Tugas/TugasB/DynamicDispatch.java
@@ -0,0 +1,23 @@
+package TugasB;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public class DynamicDispatch {
+    public static void main(String args[]){
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A ref;
+        
+        ref = b;
+        ref.callthis();
+        
+        ref = c;
+        ref.callthis();
+        
+        ref = a;
+        ref.callthis();
+    }
+}
