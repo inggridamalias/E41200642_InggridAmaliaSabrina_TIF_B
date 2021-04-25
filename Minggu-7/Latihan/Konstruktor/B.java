702219d5ba886718f93b55ccb63caeed2315f0e6
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:04:22 +0700

    Create B.java

---
+++ Minggu-7/Latihan/Konstruktor/B.java
@@ -0,0 +1,13 @@
+package Konstruktor;
+
+/**
+ *
+ * @author inggridamaliasabrina
+ */
+public class B extends A {
+    B(){
+        super("test");
+        System.out.println("Konstruktor class B dieksekusi ...");
+    }
+    
+}
