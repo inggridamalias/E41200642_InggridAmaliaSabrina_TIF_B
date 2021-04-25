7b2aff51021e5335924c3e61dfde0882f4c78da0
inggridamalias <inggridamalias99@gmail.com>
2021-04-25 14:04:28 +0700

    Create Demo.java

---
+++ Minggu-7/Latihan/Konstruktor/Demo.java
@@ -0,0 +1,12 @@
+package Konstruktor;
+
+/**
+ *
+ * @author inggridalamaliasabrina
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb;
+        subOb = new B();
+    }
+}
